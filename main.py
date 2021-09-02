117af29eba804d2ab3b3fe78f11c63f21e082af5
ihsan-salman <ihsan.salmantr@gmail.com>
2021-09-02 23:46:18 +0200

    main module and pygame window creation

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+import pygame
+
+background_colour = (0, 0, 0)
+width, height = 300, 200
+screen = pygame.display.set_mode((width, height))
+screen.fill(background_colour)
+pygame.display.flip()
+
+running = True
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
